1334c8d513a47624a5467e2398f4704d40b45dfd
Neko the cat <82635097+edison1119@users.noreply.github.com>
2021-05-12 14:58:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+command = {'>':'p=(p+1)%69420',
+           '<':'p=(p+69419)%69420',
+           '[':'while cell[p]:',
+           ']':'',
+           '+':'cell[p]+=cell[p+1]or 1',
+           '-':'cell[p]-=cell[p+1]or 1',
+           '*':'cell[p]*=cell[p+1]or 2',
+           '/':'cell[p]/=cell[p+1]or 2',
+           '.':'sys.stdout.write(chr(cell[p]))',
+           ',':'cell[p]=ord(sys.stdin.read(1))',
+           ':':'sys.stdout.write(str(cell[p]))',
+           ';':'sys.stdin.readline()'
+           }
+def run(text):
+    f = open('gener.py', 'w+')
+    f.write('import sys\ncell=[0]*69420\np=0\n')
+    s=0
+    for n in text:
+        f.write(' '*s+command[n]+'\n')
+        if n == '[':
+            s+=1
+        elif n == ']':
+            s-=1
+import gener
