d7334f014d4d4c5ab5b6b30e0f402415f1b9df95
Neko the cat <82635097+edison1119@users.noreply.github.com>
2021-05-12 16:57:51 +0800

    Update main.py
    Implemented file/input seperator `!`

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ command = {'>':'p=(p+1)%69420',
            '*':'cell[p]*=cell[p+1]or 2',
            '/':'cell[p]/=cell[p+1]or 2',
            '.':'sys.stdout.write(chr(abs(cell[p])))',
-           ',':'cell[p]=ord(sys.stdin.read(1))',
+           ',':'cell[p]=ord(sys.stdin.read(1)or"\\0")',
            ':':'sys.stdout.write(str(cell[p]))',
            ';':'cell[p]=int(sys.stdin.readline())',
            '%':'cell[p]%=cell[p+1]or 1'
            }
+import sys
+from io import StringIO
 def run(text):
     f = open('gener.py', 'w+')
     f.write('import sys\ncell=[0]*69420\np=0\n')
+    if '!' in text:
+        sys.stdin=StringIO(text.split('!')[1])
+    text = text.split('!')[0]
     s=0
     for n in text:
         f.write(' '*s+str(command.get(n))+'\n')
@@ -22,4 +27,5 @@ def run(text):
             s+=1
         elif n == ']':
             s-=1
+
 import gener
