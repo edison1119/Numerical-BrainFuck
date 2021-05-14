b703ffa0f5f2a0fb73ada7fcb597720401befe14
Neko the cat <82635097+edison1119@users.noreply.github.com>
2021-05-14 15:14:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ command = {'>':'p=(p+1)%69420',
            '<':'p=(p+69419)%69420',
            '[':'while cell[p]:',
            ']':'pass',
+           '(':'if cell[p]:',
+           ')':'pass',
            '+':'cell[p]+=cell[p+1]or 1',
            '-':'cell[p]-=cell[p+1]or 1',
            '*':'cell[p]*=cell[p+1]or 2',
@@ -18,15 +20,18 @@ import sys
 from io import StringIO
 def run(text):
     f = open('gener.py', 'w+')
-    f.write('import sys\ncell=[0]*69420\np=0\n')
+    f.write('import sys\ncell=[0]*69420\np=0\nm')
     if '!' in text:
         sys.stdin=StringIO(text.split('!')[1])
     text = text.split('!')[0]
     s=0
     for n in text:
         f.write(' '*s+str(command.get(n))+'\n')
-        if n == '[':
+        if n in'[(':
             s+=1
-        elif n == ']':
+        elif n in'])':
             s-=1
+
+if __name__ == '__main__':
+    run(input())
 import gener
